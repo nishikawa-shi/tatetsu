5cface0b94224239bec87be0e1aad7fe0d61ec5b
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2021-06-06 12:44:18 +0900

    ParticipantsUsecaseのユニットテストを追加

---
+++ test/model/usecase/participants_usecase_test.dart
@@ -0,0 +1,15 @@
+import 'package:tatetsu/model/usecase/ParticipantsUsecase.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ParticipantsUsecase', () {
+    test('getDefaults_デフォルトで3名分設定されていること', () {
+      expect(ParticipantsUsecase.getDefaults().map((e) => e.displayName),
+          equals(["Alice", "Bob", "Charley"]));
+    });
+
+    test('createDummy_Dr.から始まる名前が設定されていること', () {
+      expect(ParticipantsUsecase.createDummy().displayName, startsWith("Dr."));
+    });
+  });
+}
