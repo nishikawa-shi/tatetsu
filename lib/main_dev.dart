9c1910ae16b0824ea50c336834f445419ec5d272
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2021-07-18 10:44:55 +0900

    環境ごとのソース読み込み先を、tオプションで指定できるようにした
    flutter run --flavor dev -t lib/main_dev.dart
    flutter run --flavor prd -t lib/main_prd.dart

---
+++ lib/main_dev.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tatetsu/ui/input_participants/input_participants_page.dart';
+
+void main() {
+  runApp(Tatetsu());
+}
+
+class Tatetsu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tatetsu Dev',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const InputParticipantsPage(title: '[Dev] Input Participants'),
+    );
+  }
+}
