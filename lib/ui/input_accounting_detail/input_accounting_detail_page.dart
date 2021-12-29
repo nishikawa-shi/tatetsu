853e93c59554163bd9dcd725db252b920512513d
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2021-12-29 17:58:57 +0900

    引き回し忘れていた、会計のタイトルと値も更新されるようにした

--- lib/ui/input_accounting_detail/input_accounting_detail_page.dart
+++ lib/ui/input_accounting_detail/input_accounting_detail_page.dart
@@ -90,6 +90,9 @@ class _InputAccountingDetailPageState extends State<InputAccountingDetailPage> {
       title: TextFormField(
         initialValue: payment.data.title,
         key: UniqueKey(),
+        onChanged: (String value) {
+          payment.data.title = value;
+        },
       ),
     );
   }
@@ -144,6 +147,9 @@ class _InputAccountingDetailPageState extends State<InputAccountingDetailPage> {
       TextFormField(
         initialValue: payment.data.price.toString(),
         key: UniqueKey(),
+        onChanged: (String value) {
+          payment.data.price = double.parse(value);
+        },
       ),
     ];
   }
