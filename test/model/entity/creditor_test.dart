3faf8bacccac7ad61777f12780ef68e224457ead
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2022-01-08 10:51:46 +0900

    テストコード追加

--- test/model/entity/creditor_test.dart
+++ test/model/entity/creditor_test.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:tatetsu/model/entity/creditor.dart';
 import 'package:tatetsu/model/entity/participant.dart';
 import 'package:tatetsu/model/entity/payment.dart';
+import 'package:tatetsu/model/entity/settlement.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -8,7 +10,338 @@ void main() {
   final Participant testParticipant2 = Participant("testName2");
   final Participant testParticipant3 = Participant("testName3");
 
+  final Map<Participant, double> oneZeroEntry = {
+    testParticipant1: 0,
+  };
+  final Map<Participant, double> twoZeroEntries = {
+    testParticipant1: 0,
+    testParticipant2: 0,
+  };
+  final Map<Participant, double> oneMinusOnePlusEntries = {
+    testParticipant1: 10,
+    testParticipant2: -10,
+  };
+  final Map<Participant, double> twoMinusOnePlusEntries = {
+    testParticipant1: 20,
+    testParticipant2: -10,
+    testParticipant3: -10
+  };
+  final Map<Participant, double> oneMinusTwoPlusEntries = {
+    testParticipant1: 10,
+    testParticipant2: 10,
+    testParticipant3: -20
+  };
+  final Map<Participant, double> oneMinusOnePlusOneZeroEntries = {
+    testParticipant1: 10,
+    testParticipant2: -10,
+    testParticipant3: 0
+  };
+  final Map<Participant, double> threeZeroEntries = {
+    testParticipant1: 0,
+    testParticipant2: 0,
+    testParticipant3: 0
+  };
+
+  final List<Payment> dummyPayments = [
+    Payment(
+        title: "dummyPaymentA",
+        payer: testParticipant1,
+        price: 6000,
+        owners: {
+          testParticipant1: true,
+        })
+  ];
+
   group('Creditor', () {
+    test('extractSettlement_立替額の登録がない精算元を指定した時、nullで、プロパティに影響を与えない', () {
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = {...twoMinusOnePlusEntries};
+
+      expect(
+          testCreditor.extractSettlement(
+              from: Participant("undefined"), to: testParticipant3),
+          null);
+      expect(mapEquals(testCreditor.entries, twoMinusOnePlusEntries), true);
+    });
+
+    test('extractSettlement_立替額の登録がない精算先を指定した時、nullで、プロパティに影響を与えない', () {
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = {...twoMinusOnePlusEntries};
+
+      expect(
+          testCreditor.extractSettlement(
+              from: testParticipant1, to: Participant("undefined")),
+          null);
+      expect(mapEquals(testCreditor.entries, twoMinusOnePlusEntries), true);
+    });
+
+    test('extractSettlement_立替額が0の精算元を指定した時、nullで、プロパティに影響を与えない', () {
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = {...oneMinusOnePlusOneZeroEntries};
+
+      expect(
+          testCreditor.extractSettlement(
+              from: testParticipant3, to: testParticipant1),
+          null);
+      expect(
+          mapEquals(testCreditor.entries, oneMinusOnePlusOneZeroEntries), true);
+    });
+
+    test('extractSettlement_立替額がプラスの精算元を指定した時、nullで、プロパティに影響を与えない', () {
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = {...oneMinusTwoPlusEntries};
+
+      expect(
+          testCreditor.extractSettlement(
+              from: testParticipant1, to: testParticipant2),
+          null);
+      expect(mapEquals(testCreditor.entries, oneMinusTwoPlusEntries), true);
+    });
+
+    test('extractSettlement_立替額が0の精算先を指定した時、nullで、プロパティに影響を与えない', () {
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = {...oneMinusOnePlusOneZeroEntries};
+
+      expect(
+          testCreditor.extractSettlement(
+              from: testParticipant2, to: testParticipant3),
+          null);
+      expect(
+          mapEquals(testCreditor.entries, oneMinusOnePlusOneZeroEntries), true);
+    });
+
+    test('extractSettlement_立替額がマイナスの精算先を指定した時、nullで、プロパティに影響を与えない', () {
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = {...twoMinusOnePlusEntries};
+
+      expect(
+          testCreditor.extractSettlement(
+              from: testParticipant2, to: testParticipant3),
+          null);
+      expect(mapEquals(testCreditor.entries, twoMinusOnePlusEntries), true);
+    });
+
+    test('extractSettlement_立替費絶対値が精算先の方が大きい時、精算元基準の精算となり、プロパティに当該の精算結果が適用される',
+        () {
+      final testEntries = {
+        testParticipant1: 300.0,
+        testParticipant2: -180.0,
+        testParticipant3: -120.0,
+      };
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = testEntries;
+
+      expect(
+          testCreditor
+              .extractSettlement(from: testParticipant2, to: testParticipant1)!
+              .isEqualTo(Settlement(
+                  from: testParticipant2, to: testParticipant1, amount: 180)),
+          true);
+      expect(
+          mapEquals(testCreditor.entries, {
+            testParticipant1: 120.0,
+            testParticipant2: 0,
+            testParticipant3: -120.0
+          }),
+          true);
+    });
+
+    test(
+        'extractSettlement_立替費絶対値が精算元と精算先で同額の時、精算先基準の精算となり、プロパティに当該の精算結果が適用される',
+        () {
+      final testEntries = {
+        testParticipant1: -500.0,
+        testParticipant2: 500.0,
+        testParticipant3: 0.0,
+      };
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = testEntries;
+
+      expect(
+          testCreditor
+              .extractSettlement(from: testParticipant1, to: testParticipant2)!
+              .isEqualTo(Settlement(
+                  from: testParticipant1, to: testParticipant2, amount: 500)),
+          true);
+      expect(
+          mapEquals(testCreditor.entries,
+              {testParticipant1: 0, testParticipant2: 0, testParticipant3: 0}),
+          true);
+    });
+
+    test('extractSettlement_立替費絶対値が精算元の方が大きい時、精算先基準の精算となり、プロパティに当該の精算結果が適用される',
+        () {
+      final testEntries = {
+        testParticipant1: -500.0,
+        testParticipant2: 270.0,
+        testParticipant3: 230.0,
+      };
+      final testCreditor = Creditor(payments: dummyPayments)
+        ..entries = testEntries;
+
+      expect(
+          testCreditor
+              .extractSettlement(from: testParticipant1, to: testParticipant2)!
+              .isEqualTo(Settlement(
+                  from: testParticipant1, to: testParticipant2, amount: 270)),
+          true);
+      expect(
+          mapEquals(testCreditor.entries, {
+            testParticipant1: -230.0,
+            testParticipant2: 0,
+            testParticipant3: 230.0
+          }),
+          true);
+    });
+
+    test('getCreditors_立替額0が1人の時、空配列', () {
+      expect(
+          (Creditor(payments: dummyPayments)..entries = {...oneZeroEntry})
+              .getCreditors(),
+          []);
+    });
+
+    test('getCreditors_立替額マイナスが1人でプラスが1人の時、プラスの1人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...oneMinusOnePlusEntries})
+              .getCreditors(),
+          [testParticipant1]);
+    });
+
+    test('getCreditors_立替額0が2人の時、空配列', () {
+      expect(
+          (Creditor(payments: dummyPayments)..entries = {...twoZeroEntries})
+              .getCreditors(),
+          []);
+    });
+
+    test('getCreditors_立替額マイナスが2人でプラスが1人の時、プラスの1人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...twoMinusOnePlusEntries})
+              .getCreditors(),
+          [testParticipant1]);
+    });
+
+    test('getCreditors_立替額マイナスが1人でプラスが2人の時、プラスの2人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...oneMinusTwoPlusEntries})
+              .getCreditors(),
+          [testParticipant1, testParticipant2]);
+    });
+
+    test('getCreditors_立替額マイナスが1人でプラスが1人で0が1人の時、プラスの1人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...oneMinusOnePlusOneZeroEntries})
+              .getCreditors(),
+          [testParticipant1]);
+    });
+
+    test('getCreditors_立替額0が3人の時、空配列', () {
+      expect(
+          (Creditor(payments: dummyPayments)..entries = {...threeZeroEntries})
+              .getCreditors(),
+          []);
+    });
+
+    test('getCreditors_実行前後で、インスタンスの持つ立替者プロパティが不変', () {
+      final List<Payment> testPayments = [
+        Payment(
+            title: "testPaymentA",
+            payer: testParticipant1,
+            price: 6000,
+            owners: {
+              testParticipant1: true,
+              testParticipant2: true,
+              testParticipant3: true
+            })
+      ];
+      final Creditor testCreditor = Creditor(payments: testPayments);
+
+      expect((testCreditor..getCreditors()).entries, {
+        testParticipant1: 4000,
+        testParticipant2: -2000,
+        testParticipant3: -2000
+      });
+    });
+
+    test('getDebtors_立替額0が1人の時、空配列', () {
+      expect(
+          (Creditor(payments: dummyPayments)..entries = {...oneZeroEntry})
+              .getDebtors(),
+          []);
+    });
+
+    test('getDebtors_立替額マイナスが1人でプラスが1人の時、マイナスの1人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...oneMinusOnePlusEntries})
+              .getDebtors(),
+          [testParticipant2]);
+    });
+
+    test('getDebtors_立替額0が2人の時、空配列', () {
+      expect(
+          (Creditor(payments: dummyPayments)..entries = {...twoZeroEntries})
+              .getDebtors(),
+          []);
+    });
+
+    test('getDebtors_立替額マイナスが2人でプラスが1人の時、マイナスの2人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...twoMinusOnePlusEntries})
+              .getDebtors(),
+          [testParticipant2, testParticipant3]);
+    });
+
+    test('getDebtors_立替額マイナスが1人でプラスが2人の時、マイナスの1人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...oneMinusTwoPlusEntries})
+              .getDebtors(),
+          [testParticipant3]);
+    });
+
+    test('getDebtors_立替額マイナスが1人でプラスが1人で0が1人の時、マイナスの1人', () {
+      expect(
+          (Creditor(payments: dummyPayments)
+                ..entries = {...oneMinusOnePlusOneZeroEntries})
+              .getDebtors(),
+          [testParticipant2]);
+    });
+
+    test('getDebtors_立替額0が3人の時、空配列', () {
+      expect(
+          (Creditor(payments: dummyPayments)..entries = {...threeZeroEntries})
+              .getDebtors(),
+          []);
+    });
+
+    test('getDebtors_実行前後で、インスタンスの持つ立替者プロパティが不変', () {
+      final List<Payment> testPayments = [
+        Payment(
+            title: "testPaymentA",
+            payer: testParticipant1,
+            price: 6000,
+            owners: {
+              testParticipant1: true,
+              testParticipant2: true,
+              testParticipant3: true
+            })
+      ];
+      final Creditor testCreditor = Creditor(payments: testPayments);
+
+      expect((testCreditor..getDebtors()).entries, {
+        testParticipant1: 4000,
+        testParticipant2: -2000,
+        testParticipant3: -2000
+      });
+    });
+
     test('PaymentsExt_toCreditorEntries_Paymentが空の時、エラー', () {
       final List<Payment> testPayments = [];
 
@@ -497,4 +830,67 @@ void main() {
       expect(() => creditorEntries..apply(testPayment), throwsException);
     });
   });
+
+  group('SettlementComparer', () {
+    test('isEqualTo_支払元のインスタンスと支払先のインスタンスと支払い量が等しい時true', () {
+      final fromTestParticipant = Participant("testFromName");
+      final toTestParticipant = Participant("testToName");
+
+      expect(
+          Settlement(
+                  from: fromTestParticipant, to: toTestParticipant, amount: 30)
+              .isEqualTo(Settlement(
+                  from: fromTestParticipant,
+                  to: toTestParticipant,
+                  amount: 30)),
+          equals(true));
+    });
+
+    test('isEqualTo_支払元のインスタンスが異なる時false', () {
+      final fromTestParticipant = Participant("testFromName");
+      final toTestParticipant = Participant("testToName");
+
+      expect(
+          Settlement(
+                  from: fromTestParticipant, to: toTestParticipant, amount: 30)
+              .isEqualTo(Settlement(
+                  from: Participant("testFromName"),
+                  to: toTestParticipant,
+                  amount: 30)),
+          equals(false));
+    });
+
+    test('isEqualTo_支払先のインスタンスが異なる時false', () {
+      final fromTestParticipant = Participant("testFromName");
+      final toTestParticipant = Participant("testToName");
+
+      expect(
+          Settlement(
+                  from: fromTestParticipant, to: toTestParticipant, amount: 30)
+              .isEqualTo(Settlement(
+                  from: fromTestParticipant,
+                  to: Participant("testToName"),
+                  amount: 30)),
+          equals(false));
+    });
+
+    test('isEqualTo_支払い量が異なる時false', () {
+      final fromTestParticipant = Participant("testFromName");
+      final toTestParticipant = Participant("testToName");
+
+      expect(
+          Settlement(
+                  from: fromTestParticipant, to: toTestParticipant, amount: 30)
+              .isEqualTo(Settlement(
+                  from: fromTestParticipant,
+                  to: toTestParticipant,
+                  amount: 30.01)),
+          equals(false));
+    });
+  });
+}
+
+extension SettlementComparer on Settlement {
+  bool isEqualTo(Settlement target) =>
+      target.from == from && target.to == to && target.amount == amount;
 }
