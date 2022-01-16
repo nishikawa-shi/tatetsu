19acb6f6a89771ffb8b89b32c64eff42bb066f3f
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2022-01-16 19:13:33 +0900

    ユニットテスト表記での凡ミス修正

--- test/model/entity/creditor_test.dart
+++ test/model/entity/creditor_test.dart
@@ -321,7 +321,7 @@ void main() {
           testParticipant2: -3.552713678800501e-15,
           testParticipant3: -1e-15,
         }),
-        true,
+        equals(true),
       );
     });
 
@@ -744,7 +744,7 @@ void main() {
       expect(0.0.floorAtSecondDecimal(), equals(0));
     });
 
-    test('DoubleExt_floorAtSecondDecimal_0.01未満と判断される値が切り捨てられる、0', () {
+    test('DoubleExt_floorAtSecondDecimal_0.01未満と判断される値が切り捨てられる', () {
       expect(28.009999999999996.floorAtSecondDecimal(), equals(28));
     });
 
@@ -752,7 +752,7 @@ void main() {
       expect((-10 + 9.9900001).floorAtSecondDecimal(), equals(0));
     });
 
-    test('DoubleExt_floorAtSecondDecimal_0.01以上と判断される値は残る、0', () {
+    test('DoubleExt_floorAtSecondDecimal_0.01以上と判断される値は残る', () {
       expect(28.009999999999997.floorAtSecondDecimal(), equals(28.01));
     });
 
