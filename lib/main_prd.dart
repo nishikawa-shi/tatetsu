d169b07195b9ab783b2f6dd90e6a83dad319b383
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2022-02-06 20:45:30 +0900

    本番系ビルドにおいて、テーマカラーにアプリアイコンと同じ値が設定されるようにした

--- lib/main_prd.dart
+++ lib/main_prd.dart
@@ -11,10 +11,26 @@ class Tatetsu extends StatelessWidget {
     return MaterialApp(
       title: 'Tatetsu',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: _tatetsuViolet,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const InputParticipantsPage(title: 'Participants'),
     );
   }
+
+  static const MaterialColor _tatetsuViolet = MaterialColor(
+    0xFF8489B6,
+    {
+      50: Color(0xFFF0F1F6),
+      100: Color(0xFFDADCE9),
+      200: Color(0xFFC2C4DB),
+      300: Color(0xFFA9ACCC),
+      400: Color(0xFF969BC1),
+      500: Color(0xFF8489B6),
+      600: Color(0xFF7C81AF),
+      700: Color(0xFF7176A6),
+      800: Color(0xFF676C9E),
+      900: Color(0xFF54598E),
+    },
+  );
 }
