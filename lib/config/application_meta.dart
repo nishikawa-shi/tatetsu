15abeffab4cfdab7e59631fd3b7fb3eaab516663
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2022-02-11 22:15:35 +0900

    dev/prd Google/Google以外 で異なる精算画面バナー用ユニットIDを取得できる機構を追加

--- lib/config/application_meta.dart
+++ lib/config/application_meta.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_flavor/flutter_flavor.dart';
 import 'package:tatetsu/config/core.dart';
@@ -26,5 +28,18 @@ String getEntryPageTitle() {
     return FlavorConfig.instance.variables["entry_page_title"] as String;
   } catch (e) {
     return "Participants";
+  }
+}
+
+String getSettleAccountsTopBannerId() {
+  try {
+    return (Platform.isAndroid || Platform.isFuchsia)
+        ? FlavorConfig.instance
+            .variables["settle_accounts_top_banner_id_android"] as String
+        : FlavorConfig.instance.variables["settle_accounts_top_banner_id_ios"]
+            as String;
+  } catch (e) {
+    // テスト広告のID https://developers.google.com/admob/ios/test-ads#demo_ad_units
+    return "ca-app-pub-3940256099942544/2934735716";
   }
 }
--- lib/config/sample_env.dart
+++ lib/config/sample_env.dart
@@ -9,6 +9,10 @@ void setConfig() => FlavorConfig(
           primarySwatch: Colors.red,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        "entry_page_title": "title"
+        "entry_page_title": "title",
+        "settle_accounts_top_banner_id_ios":
+            "ca-app-pub-3940256099942544/2934735716",
+        "settle_accounts_top_banner_id_android":
+            "ca-app-pub-3940256099942544/2934735716"
       },
     );
