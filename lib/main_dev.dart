7eeb9b47dc0324356794088473c51e576682ad9f
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2022-02-20 17:29:04 +0900

    l10nしたテキストが利用できるようにした

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tatetsu/config/application_meta.dart';
 import 'package:tatetsu/config/dev.dart';
+import 'package:tatetsu/l10n/built/app_localizations.dart';
 import 'package:tatetsu/model/usecase/advertisement_usecase.dart';
 import 'package:tatetsu/ui/input_participants/input_participants_page.dart';
 
@@ -18,6 +19,7 @@ class Tatetsu extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
         localizationsDelegates: const [
+          AppLocalizations.delegate,
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
           GlobalCupertinoLocalizations.delegate,
