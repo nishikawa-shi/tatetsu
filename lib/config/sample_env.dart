e68cc96d20e258a5076f4f56c32b32df2ed4b786
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2023-01-07 09:48:35 +0900

    フローティングアクションメニュー子要素用のカラースキームを設定

--- lib/config/sample_env.dart
+++ lib/config/sample_env.dart
@@ -9,7 +9,9 @@ void setConfig() => FlavorConfig(
         "application_theme": ThemeData(
           colorScheme: const ColorScheme.light(
             primary: Colors.red,
+            secondary: Colors.pinkAccent,
             onSurface: Colors.grey, // これを設定しないと、非活性状態アイコンと入力域下線の基準色にデフォルト値黒が設定され、アプリテーマカラーの他アイコンとのコントラストが低くなってしまう
+            onSecondary: Colors.white,
           ),
           primarySwatch: Colors.red, // これを設定しないとチェックボックス(立替参加者除外画面)の色にprimaryが設定されない
           disabledColor: tatetsuGrey, // これを設定しないと、編集破棄ダイアログキャンセル文字色にデフォルト値濃いめ灰色が設定され、アプリテーマカラーの破棄ボタンとのコントラストが低くなってしまう
