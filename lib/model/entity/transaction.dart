d533da792b014fefd2c2d8450d723431eae4ab10
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2025-01-19 12:36:34 +0900

    whereNotNullをnonNullの記述に移行した

--- lib/model/entity/transaction.dart
+++ lib/model/entity/transaction.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:tatetsu/l10n/built/app_localizations.dart';
@@ -89,7 +88,7 @@ extension CreditorExt on Creditor {
               to: creditor,
             ),
           )
-          .whereNotNull()
+          .nonNulls
           .toList();
 }
 
